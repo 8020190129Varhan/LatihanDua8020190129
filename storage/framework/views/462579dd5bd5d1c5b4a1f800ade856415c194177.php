9b041ee5255e866d86c738998e43e741e3f4c9bc
HannRubyJane <94168249+8020190129Varhan@users.noreply.github.com>
2022-01-22 21:22:55 +0700

    LatihanDua 8020190129

---
+++ storage/framework/views/462579dd5bd5d1c5b4a1f800ade856415c194177.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    </head>
+    <body>
+      <div class="container-md">
+
+        <form action="/buku/<?php echo e($data->id); ?>/update"method=POST>
+            <?php echo e(csrf_field()); ?>
+
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Judul Buku</label>
+              <input name="judul" type="text" value="<?php echo e($data->judul); ?>" class="form-control" id="judul" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Nama Pengarang</label>
+              <input name= "pengarang" type="text" value="<?php echo e($data->pengarang); ?>" class="form-control" id="pengarang">
+            </div>
+            <div class="mb-3 form-check">
+            </div>
+            <button type="submit" class="btn btn-info">Update</button>
+          </form>
+           </div>
+
+        <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
+<?php /**PATH C:\Users\myudh\Documents\History\LatihanDua80201901029\resources\views/Buku/edit.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f8033fe8bbb14f07c317f7cbd64b804f104f6e2e.php
@@ -0,0 +1,11 @@
+<h1>Create</h1>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    </head>
+    <body>
+        
+    </body><?php /**PATH C:\Users\myudh\Documents\Latihan Pvisual\LatihanDua8020190107\resources\views/Buku/create.blade.php ENDPATH**/ ?>
